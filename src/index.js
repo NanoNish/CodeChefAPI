e7fa76810db54d39433cdac3696c539054791e99
NanoNish <nishkarsh1206@gmail.com>
2023-02-21 04:57:07 +0530

    chore: format all files using prettier

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 // const express = require('express');
-import express from 'express';
+import express from "express";
 
 // problem = require('./problem');
-import {api as api} from './codechef/api.js';
-import problem from './problem.js';
+import { api as api } from "./codechef/api.js";
+import problem from "./problem.js";
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hey There!!!');
-})
+app.get("/", (req, res) => {
+  res.send("Hey There!!!");
+});
 
-app.get('/problem/:problemId', async (req, res) => {
-    var problemStatement = await problem(req.params.problemId);
-    res.send(problemStatement);
-})
+app.get("/problem/:problemId", async (req, res) => {
+  var problemStatement = await problem(req.params.problemId);
+  res.send(problemStatement);
+});
 
-app.use('/api', api);
+app.use("/api", api);
 
 app.listen(port, () => {
-    console.log(`The app is listening on port ${port}`);
-})
+  console.log(`The app is listening on port ${port}`);
+});
--- src/problem.js
+++ src/problem.js
@@ -1,19 +1,21 @@
-import puppeteer from 'puppeteer';
+import puppeteer from "puppeteer";
 
 export default async function problem(problemId) {
-    const start = Date.now();
+  const start = Date.now();
 
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // console.log('https://www.codechef.com/problems/'+problemId);
-    await page.goto(`https://www.codechef.com/problems/${problemId}`, {
-        waitUntil: 'networkidle0',
-    });
-    // console.log("Page loaded");
-    const problem = await page.evaluate(() => document.getElementById('problem-statement')?.innerHTML.toString());
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  // console.log('https://www.codechef.com/problems/'+problemId);
+  await page.goto(`https://www.codechef.com/problems/${problemId}`, {
+    waitUntil: "networkidle0",
+  });
+  // console.log("Page loaded");
+  const problem = await page.evaluate(() =>
+    document.getElementById("problem-statement")?.innerHTML.toString()
+  );
 
-    const end = Date.now();
-    console.log(`Time taken to fetch problem statement: ${end - start}ms`);
+  const end = Date.now();
+  console.log(`Time taken to fetch problem statement: ${end - start}ms`);
 
-    return problem;
+  return problem;
 }
