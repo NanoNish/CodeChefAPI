9a6f3509f430b6cc5042cd2e82249a79c5df402a
NanoNish <nishkarsh1206@gmail.com>
2023-02-21 04:54:26 +0530

    feat: add support for official codechef api
    - support for both porblems and contests from the official codechef api
    - add an api module for above

---
+++ src/codechef/api.js
@@ -0,0 +1,26 @@
+import { Router } from 'express';
+import axios from 'axios';
+
+const api = Router();
+
+api.get('/', (req, res) => {
+    res.send('You are here on API route');
+})
+
+api.get('/problem/:problemId', async (req, res) => {
+    var problem = await axios.get(`https://www.codechef.com/api/contests/PRACTICE/problems/${req.params.problemId}`).then(
+        (response) => {
+            return response.data;
+        }
+    );
+    res.send(problem);
+})
+
+api.get('/contest/:contestId', async (req, res) => {
+    var contest = await axios.get(`https://www.codechef.com/api/contests/${req.params.contestId}`).then((response) => {
+        return response.data;
+        });
+    res.send(contest);
+})
+
+export { api };
